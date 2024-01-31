cc74d9c1e9e020bd532307a9a8c4fe35e44bf01f
davideCaligola <dvd.c@libero.it>
2024-01-31 13:06:44 +0100

    updated simulation model for reaching according to lab specifications

--- 2023a/reaching_init.m
+++ 2023a/reaching_init.m
@@ -2,6 +2,7 @@ clear all;
 
 % fixed step time
 time_step = 0.05;
+Ts = time_step;
 
 % threshold for reaching target / home center
 th = 0.005;
@@ -10,12 +11,12 @@ th = 0.005;
 n_target = 8;
 radius = 0.1;
 coord_home = [0,0];
-coord_targets = zeros(n_target,2);
+coord_targets = zeros(n_target+2,2);
 step = 2*pi/n_target;
 for i=0:n_target-1
     coord_targets(i+1,:) = [radius*cos(i*step), radius*sin(i*step)];
 end
 
 % randomize coordiantes order
-rand_idx = randperm(n_target);
-coord_targets(rand_idx,:) = coord_targets;
+rand_idx = randi(n_target,[1,10]);
+coord_targets = coord_targets(rand_idx,:);
